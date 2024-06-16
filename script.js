01cc84248471a3c992f05faf7afe7d76ab71dce5
Hugo Leonardo <60386233+HugoLeonardoH1@users.noreply.github.com>
2024-06-16 00:29:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+// // 1
+// alert("Hello World")
+// // 2
+// let dado1 = Number(prompt("Digite um numero!"))
+// let dado2 = Number(prompt("Digite um numero!"))
+// alert(`Os numeros são ${dado1}e ${dado2}`)
+
+// 3
+// let teste1 = 5;
+// let test2 ="texto";
+
+// if(typeof test2 === 'number'){
+//   alert("É um numero")
+// }else{
+//   alert("Não é um numero")
+// }
+
+//4
+
+// let teste4 = 3;
+// let teste5 = "texto"
+
+
+// if(typeof teste5 === 'string'){
+//   alert("É um Texto")
+// }else{
+//   alert("Não é um Texto")
+// }
+
+
+//5
+
+// let teste6 = true;
+// let teste7 = "texto"
+
+
+// if(typeof teste6 === 'boolean'){
+//   alert("It's a  boolean")
+// }else{
+//   alert("Isn`t a boolean")
+// }
+
+
+//6
+let num1 = 5
+let num2 = 6
+
+let sub = num1 + num2
+alert(sub)
+
+//7
+
+let num3 = 5
+let num4 = 6
+
+let plus = num1 * num2
+alert(sub)
+//8
+
+let num5 = 5
+let num6 = 6
+
+let div = num1 / num2
+alert(sub)
+
+//9
+let isPair = 6;
+if(isPair % 2 === 0  ) {
+  alert("Its pair")
+}else{
+  alert("Isnt Pair")
+}
+
+//10
+let isntPair = 6;
+if(isPair % 2 === 0  ) {
+  alert("Nao é impar ")
+}else{
+  alert("É par")
+}
